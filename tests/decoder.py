0ce19a42d922dd1bcfa42775c7a996e2a188d572
james <jameslryan07@gmail.com>
2025-12-16 15:31:14 -0500

    ya :(

--- tests/decoder.py
+++ tests/decoder.py
@@ -10,3 +10,13 @@ import cocotb # this wont work anywhere yikes
     # Set input port to be the instruction containing the 12 bit number 
 # Wait for clock next clock cycle
     # Check if output port is the expected 32 bit number, sign extended from the 12 bit number
+
+
+from cocotb.clock import Clock # very nice trigger
+from cocotb.triggers import RisingEdge, ReadOnly, Timer, ReadWrite # cocotb stuff
+import logging # learn how to use this
+import random as rd
+
+@cocotb.test()
+async def decoder_test(dut):
+    pass
