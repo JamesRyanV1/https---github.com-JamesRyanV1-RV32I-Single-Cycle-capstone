dd0a6b0f1aef37490cdcb22420c8cf4540fa2986
Jamesv2 <Jameslryan07@gmail.com>
2025-12-16 18:06:03 -0500

    oop

--- tests/registerFile.py
+++ tests/registerFile.py
@@ -12,13 +12,13 @@ from util import random_binary # random binary function from my utils
 async def register_file_test(dut):
 
     # start a  cocotb-driven clock (10 ns period)
-    cocotb.start_soon(Clock(dut.clk, 10, unit="ns").start())
+    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
     dut.rst.value = 1
-    dut.rs1.value = 0
-    dut.rs2.value = 0
-    dut.rd.value = 0
-    dut.wd.value = 0 
-    dut.enableWrite.value = 1
+    dut.rs1 = 0
+    dut.rs2 = 0
+    dut.rd = 0
+    dut.wd = 0 
+    dut.enableWrite
 
 
     # turns off reset
@@ -39,38 +39,38 @@ async def register_file_test(dut):
         write_value = random_binary(32)
 
         #reads
-        await Timer(1,unit="ns")
+        await Timer(1,units="ns")
         dut.rs1.value = ad1
         dut.rs2.value = ad2
-        await Timer(1,unit="ns")
+        await Timer(1,units="ns")
         # makes sure the data being read is the same as the data that should be in the regs
         # data that should be there represented by the stand in regs, (im not making a path between this and memory for the testbench)
         assert dut.rd1.value == fake_regs[ad1]
         assert dut.rd2.value == fake_regs[ad2]
 
         #random writes, stored then read when loops
-        dut.wd.value = ad3 # WAS DUT.AD3.VALUE, FIX AND FIGURE OUT WHAT THIS IS SUPPOSED TO BE
+        dut.ad3.value = ad3
         dut.enableWrite.value = 1 #enables write for this portion
-        dut.wd.value = write_value
+        dut.wd = write_value
         await RisingEdge(dut.clk)
         dut.enableWrite.value = 0
         fake_regs[ad3] = write_value
-        await Timer(1,unit="ns")
+        await Timer(1,units="ns")
         
     # Try and write to 0, after loop bc would unsettle the zerod fake regs
-    await Timer(1, unit="ns")
+    await Timer(1, units="ns")
     dut.ad3.value = 0
     dut.enableWrite.value=1
     dut.wd = random_binary(32)
     await RisingEdge(dut.clk)
     dut.wd.value = 0
     fake_regs[ad3] = write_value
-    await Timer(1, unit="ns")
+    await Timer(1, units="ns")
 
     # rd1 (if 0) should end up as 0, final assertion after trying to change it
-    await Timer(1, unit="ns")
-    dut.d1.value = 0
-    await Timer(1, unit="ns")
+    await Timer(1, units="ns")
+    dut.ad1.value = 0
+    await Timer(1, units="ns")
     assert int(dut.rd1.value) == 0
 
     # if random read/writtes work, and 0 cannot be changed from 0, cpu works
